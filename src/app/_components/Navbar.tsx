d2f0906a9ed14db369e59fee5122dca0d2c3e831
dr-george-dev <labegeorgeedu@gmail.com>
2026-01-12 11:04:42 +0100

    add a login and logout functions using true and fals

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,14 +1,38 @@
 import Link from 'next/link'
 import React from 'react'
+import LogoutButton from './LogoutButton';
 
 const Navbar = () => {
+    const session = false;
   return (
     <nav className='bg-white shadow-sm'>
         <div className="container mx-auto p-4 flex justify-between items-center">
             <Link href="/" className='text-xl font-bold text-blue-600'>
                 Contact Manager
                 </Link>
+      
+
+        <div className="flex items-center space-x-4">
+            {session ? (
+                <>
+                <Link href="/contact" className='text-gray-700 hover:text-blue-600 mr-8'>
+                Contacts
+                </Link>
+                <LogoutButton />
+                </>
+            ) : (
+
+                <>
+                <Link href="/login" className='text-gray-700 hover:text-blue-600 mr-5'>
+                Login
+                </Link>
+                <Link href="/register" className='text-gray-700 hover:text-blue-600'>
+                Register
+                </Link>
+                </>
+            )}
         </div>
+          </div>
 
     </nav>
   )
